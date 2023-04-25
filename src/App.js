86ea8cea1012f1133ef901e39c417ee3fe419bca
svloc <svloc39@gmail.com>
2023-04-26 00:07:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-gray-100">
+      <h1 className="text-3xl font-bold text-center py-4">Hello Tailwind CSS!</h1>
+      <p className="text-gray-700 text-lg text-center">Tailwind CSS is awesome!</p>
+    </div>
+  );
+}
+
+export default App;
